f2d9b8c788a50ffa635e592b957b56e60240ea11
Andrew Glines <aglines-epicodus@gmail.com>
2017-06-06 07:44:34 -0700

    add initial code - db connection string, project.json framework, Startup.cs conn string, database object in Database.cs

---
+++ Startup.cs
@@ -0,0 +1,47 @@
+using System.IO;
+using Microsoft.AspNet.Builder;
+using Nancy;
+using Nancy.Owin;
+using Nancy.ViewEngines.Razor;
+using System.Collections.Generic;
+using System;
+
+namespace ToDoList
+{
+  public static class DBConfiguration
+  {
+    public static string ConnectionString = "Data Source=(localdb)\\mssqllocaldb;InitialCatalog=todo;Integrated Security=SSPI";
+  }
+  
+  }
+
+  public class Startup
+  {
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseOwin(x => x.UseNancy());
+    }
+  }
+  public class CustomRootPathProvider : IRootPathProvider
+  {
+    public string GetRootPath()
+    {
+      return Directory.GetCurrentDirectory();
+    }
+  }
+  public class RazorConfig : IRazorConfiguration
+  {
+    public IEnumerable<string> GetAssemblyNames()
+    {
+      return null;
+    }
+    public IEnumerable<string> GetDefaultNamespaces()
+    {
+      return null;
+    }
+    public bool AutoIncludeModelNamespace
+    {
+      get {return false;}
+    }
+  }
+}
